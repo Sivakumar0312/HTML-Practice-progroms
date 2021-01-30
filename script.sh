8d3db9d9cfd60698a2501daa55a2ed0f11eb5cbe
Sivakumar0312 <78156874+Sivakumar0312@users.noreply.github.com>
2021-01-30 17:56:25 +0530

    Commit@3

--- script.sh
+++ script.sh
@@ -1,1 +1,4 @@
 commenting for github
+A screenplay, or script, is a written work by screenwriters for a film, television program, or video game.
+These screenplays can be original works or adaptations from existing pieces of writing. 
+In them, the movement, actions, expression and dialogues of the characters are also narrated.
